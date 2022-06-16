098d109020aa86dbcdc0465caf3914ffa4ff83ff
mori yuta <59682979+utamori@users.noreply.github.com>
2022-06-16 22:05:14 +0900

    Create notranslate.js

---
+++ chrome/notranslate.js
@@ -0,0 +1,14 @@
+const selectors = [
+  "adiv.prism-code",
+  "div.enlighter-overflow-scroll",
+  "div.code-excerpt",
+  "pre",
+  'div[spellcheck="false"]',
+  "button",
+];
+
+const targets = document.querySelectorAll(selectors.join());
+
+for (const target of targets) {
+  target.classList.add("notranslate");
+}
